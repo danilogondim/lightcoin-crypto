cf674b077f4d97142b34535cb01988bc39d23e7f
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-28 23:12:34 +0000

    Refacoted the commit method

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ class Account {
     // Have the account balance start at $0 since that makes more sense.
     this.balance = 0;
   }
-
 }
 
 class Transaction {
@@ -21,20 +20,23 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
@@ -43,20 +45,14 @@ class Withdrawal extends Transaction {
 
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-const t1 = new Withdrawal(50.25, myAccount);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-const t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', myAccount.balance);
-
-const t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
-console.log('Balance:', myAccount.balance);
+console.log('Ending Balance:', myAccount.balance);
